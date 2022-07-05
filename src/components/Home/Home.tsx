4db6272b8b0dfb1181bde6f77246686917e57fd4
chungnguyen <chungnguyen10012000@gmail.com>
2022-07-05 21:32:10 +0700

    edit home for admin

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,6 +6,57 @@ import { IProductState, IStateType } from "../../store/models/root.interface";
 import ProductList from "../Products/ProductsList";
 import { IOrder } from "../../store/models/order.interface";
 import OrderList from "../Orders/OrderList";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Doanh thu',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 const Home: React.FC = () => {
   const products: IProductState = useSelector((state: IStateType) => state.products);
@@ -18,22 +69,28 @@ const Home: React.FC = () => {
   const totalOrderAmount: number = orders.reduce((prev, next) => prev + next.amount, 0);
 
   const dispatch: Dispatch<any> = useDispatch();
-  dispatch(updateCurrentPath("home", ""));
+  dispatch(updateCurrentPath("Trang chủ", ""));
 
   return (
     <Fragment>
-      <h1 className="h3 mb-2 text-gray-800">Dashboard</h1>
-      <p className="mb-4">Summary and overview of our admin stuff here</p>
+      <h1 className="h3 mb-2 text-gray-800">Trang chủ</h1>
+      {/* <p className="mb-4">Summary and overview of our admin stuff here</p> */}
 
       <div className="row">
-        <TopCard title="PRODUCT COUNT" text={`${numberItemsCount}`} icon="box" class="primary" />
-        <TopCard title="PRODUCT AMOUNT" text={`${totalProductAmount}`} icon="warehouse" class="danger" />
-        <TopCard title="SUMMARY PRICE" text={`$${totalPrice}`} icon="dollar-sign" class="success" />
+        <TopCard title="KHÓA HỌC" text={`${numberItemsCount}`} icon="box" class="primary" />
+        <TopCard title="CUỘC THI" text={`${totalProductAmount}`} icon="warehouse" class="danger" />
+        <TopCard title="DOANH THU" text={`$${totalPrice}`} icon="dollar-sign" class="success" />
       </div>
 
       <div className="row">
-        <TopCard title="SALES" text={totalSales.toString()} icon="donate" class="primary" />
-        <TopCard title="ORDER AMOUNT" text={totalOrderAmount.toString()} icon="calculator" class="danger" />
+        <TopCard title="HỌC VIÊN" text={totalSales.toString()} icon="donate" class="primary" />
+        <TopCard title="GIÁO VIÊN" text={totalOrderAmount.toString()} icon="calculator" class="danger" />
+      </div>
+
+      <div className="row">
+        <div className="col-xl-12 col-lg-12">
+          <Bar options={options} data={data} />
+        </div>
       </div>
 
       <div className="row">
@@ -41,7 +98,7 @@ const Home: React.FC = () => {
         <div className="col-xl-6 col-lg-6">
           <div className="card shadow mb-4">
             <div className="card-header py-3">
-              <h6 className="m-0 font-weight-bold text-green">Product list</h6>
+              <h6 className="m-0 font-weight-bold text-green">Top khóa học được yêu thích</h6>
             </div>
             <div className="card-body">
               <ProductList />
@@ -53,7 +110,7 @@ const Home: React.FC = () => {
         <div className="col-xl-6 col-lg-6">
           <div className="card shadow mb-4">
             <div className="card-header py-3">
-              <h6 className="m-0 font-weight-bold text-green">Order list</h6>
+              <h6 className="m-0 font-weight-bold text-green">Top khóa học ít người đăng ký</h6>
             </div>
             <div className="card-body">
               <OrderList />

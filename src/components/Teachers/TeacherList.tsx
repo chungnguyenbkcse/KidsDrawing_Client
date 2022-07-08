3078fcd92ff9763cb3970d3e68df7947249ecaa8
chungnguyen <chungnguyen10012000@gmail.com>
2022-07-08 10:03:23 +0700

    fix edit button

--- src/components/Teachers/TeacherList.tsx
+++ src/components/Teachers/TeacherList.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { Dispatch, Fragment, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { IStateType, IProductState } from "../../store/models/root.interface";
-import { IProduct } from "../../store/models/product.interface";
 import { useHistory } from "react-router-dom";
+import Popup from "reactjs-popup";
+import {
+    removeProduct, clearSelectedProduct, setModificationState,
+    changeSelectedProduct
+} from "../../store/actions/products.action";
+import { ProductModificationStatus, IProduct } from "../../store/models/product.interface";
+import TeacherForm from "./TeacherForm";
 
 export type productListProps = {
   onSelect?: (product: IProduct) => void;
@@ -34,25 +40,37 @@ const data = [
 ]
 
 function TeacherList(props: productListProps): JSX.Element  {
+  
+  const dispatch: Dispatch<any> = useDispatch();
+
   const products: IProductState = useSelector((state: IStateType) => state.products);
   const history = useHistory();
+
+  const [popup, setPopup] = useState(false);
   
   const routeChange = () =>{ 
     let path = '/teachers/detail'; 
     history.push(path);
   }
 
+  function onProductRemove() {
+    setPopup(true);
+}
+
   const productElements: (JSX.Element | null)[] = data.map(product => {
     if (!product) { return null; }
     return (<tr className={`table-row ${(products.selectedProduct && products.selectedProduct.id === product.id) ? "selected" : ""}`}
-      onClick={routeChange}
+      
       key={`product_${product.id}`}>
       <th scope="row">{product.id}</th>
-      <td>{product.first_name} {product.last_name}</td>
+      <td onClick={routeChange}>{product.first_name} {product.last_name}</td>
       <td>{product.username}</td>
       <td>{product.status}</td>
       <td>
-        <button type="button" className="btn btn-primary">Chỉnh sửa</button>
+        <button type="button" className="btn btn-primary" onClick={() => {
+          dispatch(setModificationState(ProductModificationStatus.Create))
+          onProductRemove()
+        }}>Chỉnh sửa</button>
       </td>
       <td>
         <button type="button" className="btn btn-danger">Xóa</button>
@@ -62,6 +80,7 @@ function TeacherList(props: productListProps): JSX.Element  {
 
 
   return (
+    <Fragment>
     <div className="table-responsive portlet">
       <table className="table">
         <thead className="thead-light">
@@ -79,6 +98,16 @@ function TeacherList(props: productListProps): JSX.Element  {
         </tbody>
       </table>
     </div>
+    <Popup
+                open={popup}
+                onClose={() => setPopup(false)}
+                closeOnDocumentClick
+            >
+                <div className="row text-left">
+                    <TeacherForm />
+                </div>
+            </Popup>
+    </Fragment>
 
   );
 }

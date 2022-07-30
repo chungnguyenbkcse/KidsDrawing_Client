76a6aa51b30d6d2f60014cf3b16dac044646a059
chungnguyen <chungnguyen10012000@gmail.com>
2022-07-31 00:51:16 +0700

    add left menu for role

--- src/components/LeftMenu/LeftMenu.tsx
+++ src/components/LeftMenu/LeftMenu.tsx
@@ -15,12 +15,294 @@ const LeftMenu: React.FC = () => {
 
     let [leftMenuVisibility, setLeftMenuVisibility] = useState(false);
 
+    var role_privilege = localStorage.getItem('role_privilege')
+    var rolePrivilege:string[] =[]
+    var roleUser :string =""
+    if (role_privilege !== null) {
+        rolePrivilege = role_privilege.split(',')
+        roleUser = rolePrivilege[0]
+    }
+
     function changeLeftMenuVisibility() {
         setLeftMenuVisibility(!leftMenuVisibility);
     }
 
     function getCollapseClass() {
         return (leftMenuVisibility) ? "" : "collapsed";
+    }
+
+    if (roleUser === 'ADMIN_USER'){
+        return (
+            <Fragment>
+                <div className="toggle-area">
+                    <button className="btn btn-primary toggle-button" onClick={() => changeLeftMenuVisibility()}>
+                        <i className="fas fa-bolt"></i>
+                    </button>
+                </div>
+    
+                <ul className={`navbar-nav bg-gradient-primary-green sidebar sidebar-dark accordion ${getCollapseClass()}`}
+                    id="collapseMenu">
+    
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div className="sidebar-brand-icon icon-green rotate-n-15">
+                            <i className="fas fa-bolt"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">Kids <sup>Drawing</sup></div>
+                    </a>
+    
+                    <hr className="sidebar-divider my-0" />
+    
+                    <li className="nav-item active">
+    
+                        <Link className="nav-link" to="/Home">
+                            <i className="fas fa-fw fa-tachometer-alt"></i>
+                            <span>Trang chủ</span>
+                        </Link>
+                    </li>
+    
+                    <hr className="sidebar-divider" />
+                    <div className="sidebar-heading">
+                        Quản lý người dùng
+                    </div>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/teachers`}>
+                            <FaChalkboardTeacher />
+                            <span> Giáo viên</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/students`}>
+                            <FaChild />
+                            <span> Học sinh</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/parents`}>
+                            <RiParentFill />
+                            <span> Phụ huynh</span>
+                        </Link>
+                    </li>
+    
+                    <hr className="sidebar-divider" />
+    
+                    <div className="sidebar-heading">
+                        Quản lý hệ thống
+                    </div>
+    
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/semesters`}>
+                            <BsMap />
+                            <span> Học kì</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/lessons`}>
+                            <AiOutlineBook />
+                            <span> Tiết học</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/schedules`}>
+                            <AiFillCalendar />
+                            <span> Lịch học</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/arts`}>
+                            <FaPaintBrush />
+                            <span> Nghệ thuật</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/courses`}>
+                            <MdSchool />
+                            <span> Khóa học</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/class`}>
+                            <GiVideoConference />
+                            <span> Lớp</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/contests`}>
+                            <AiOutlineFileText />
+                            <span> Cuộc thi</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/turnovers`}>
+                            <BsCashCoin />
+                            <span> Doanh thu</span>
+                        </Link>
+                    </li>
+    
+                    <hr className="sidebar-divider" />
+    
+                    {/* <div className="sidebar-heading">
+                        Trò chơi
+                    </div>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/game`}>
+                            <TbDeviceGamepad2 />
+                            <span> Game nhận biết màu sắc</span>
+                        </Link>
+                    </li> */}
+    
+                    <hr className="sidebar-divider" />
+    
+                    <div className="sidebar-heading">
+                        Yêu cầu người dùng
+                    </div>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/request-student-off`}>
+                            <span>Nghỉ học</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/request-teacher-off`}>
+                            <span>Nghỉ dạy</span>
+                        </Link>
+                    </li>
+    
+                    <hr className="sidebar-divider d-none d-md-block" />
+                </ul>
+            </Fragment>
+        );
+    }
+    
+    else if (roleUser === 'TEACHER_USER') {
+        return (
+            <Fragment>
+                <div className="toggle-area">
+                    <button className="btn btn-primary toggle-button" onClick={() => changeLeftMenuVisibility()}>
+                        <i className="fas fa-bolt"></i>
+                    </button>
+                </div>
+    
+                <ul className={`navbar-nav bg-gradient-primary-green sidebar sidebar-dark accordion ${getCollapseClass()}`}
+                    id="collapseMenu">
+    
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div className="sidebar-brand-icon icon-green rotate-n-15">
+                            <i className="fas fa-bolt"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">Kids <sup>Drawing</sup></div>
+                    </a>
+    
+                    <hr className="sidebar-divider my-0" />
+    
+                    <li className="nav-item active">
+    
+                        <Link className="nav-link" to="/Home">
+                            <i className="fas fa-fw fa-tachometer-alt"></i>
+                            <span>Trang chủ</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/courses`}>
+                            <MdSchool />
+                            <span> Khóa học</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/class`}>
+                            <GiVideoConference />
+                            <span> Lớp</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/contests`}>
+                            <AiOutlineFileText />
+                            <span> Cuộc thi</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/turnovers`}>
+                            <BsCashCoin />
+                            <span> Thời khóa biểu</span>
+                        </Link>
+                    </li>
+    
+                    <hr className="sidebar-divider d-none d-md-block" />
+                </ul>
+            </Fragment>
+        );
+    }
+
+    else if (roleUser === 'PARENT_USER') {
+        return (
+            <Fragment>
+                <div className="toggle-area">
+                    <button className="btn btn-primary toggle-button" onClick={() => changeLeftMenuVisibility()}>
+                        <i className="fas fa-bolt"></i>
+                    </button>
+                </div>
+    
+                <ul className={`navbar-nav bg-gradient-primary-green sidebar sidebar-dark accordion ${getCollapseClass()}`}
+                    id="collapseMenu">
+    
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div className="sidebar-brand-icon icon-green rotate-n-15">
+                            <i className="fas fa-bolt"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">Kids <sup>Drawing</sup></div>
+                    </a>
+    
+                    <hr className="sidebar-divider my-0" />
+    
+                    <li className="nav-item active">
+    
+                        <Link className="nav-link" to="/Home">
+                            <i className="fas fa-fw fa-tachometer-alt"></i>
+                            <span>Trang chủ</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/students`}>
+                            <FaChild />
+                            <span> Khóa học</span>
+                        </Link>
+                    </li>
+    
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/parents`}>
+                            <RiParentFill />
+                            <span> Cuộc thi</span>
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" to={`/turnovers`}>
+                            <BsCashCoin />
+                            <span> Thời khóa biểu</span>
+                        </Link>
+                    </li>
+    
+                    <hr className="sidebar-divider d-none d-md-block" />
+                </ul>
+            </Fragment>
+        );
     }
 
     return (
@@ -51,84 +333,16 @@ const LeftMenu: React.FC = () => {
                     </Link>
                 </li>
 
-                <hr className="sidebar-divider" />
-                <div className="sidebar-heading">
-                    Quản lý người dùng
-                </div>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/teachers`}>
-                        <FaChalkboardTeacher />
-                        <span> Giáo viên</span>
-                    </Link>
-                </li>
-
                 <li className="nav-item">
                     <Link className="nav-link" to={`/students`}>
                         <FaChild />
-                        <span> Học sinh</span>
+                        <span> Khóa học</span>
                     </Link>
                 </li>
 
                 <li className="nav-item">
                     <Link className="nav-link" to={`/parents`}>
                         <RiParentFill />
-                        <span> Phụ huynh</span>
-                    </Link>
-                </li>
-
-                <hr className="sidebar-divider" />
-
-                <div className="sidebar-heading">
-                    Quản lý hệ thống
-                </div>
-
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/semesters`}>
-                        <BsMap />
-                        <span> Học kì</span>
-                    </Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/lessons`}>
-                        <AiOutlineBook />
-                        <span> Tiết học</span>
-                    </Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/schedules`}>
-                        <AiFillCalendar />
-                        <span> Lịch học</span>
-                    </Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/arts`}>
-                        <FaPaintBrush />
-                        <span> Nghệ thuật</span>
-                    </Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/courses`}>
-                        <MdSchool />
-                        <span> Khóa học</span>
-                    </Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/class`}>
-                        <GiVideoConference />
-                        <span> Lớp</span>
-                    </Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/contests`}>
-                        <AiOutlineFileText />
                         <span> Cuộc thi</span>
                     </Link>
                 </li>
@@ -136,38 +350,14 @@ const LeftMenu: React.FC = () => {
                 <li className="nav-item">
                     <Link className="nav-link" to={`/turnovers`}>
                         <BsCashCoin />
-                        <span> Doanh thu</span>
-                    </Link>
-                </li>
-
-                <hr className="sidebar-divider" />
-
-                {/* <div className="sidebar-heading">
-                    Trò chơi
-                </div>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/game`}>
-                        <TbDeviceGamepad2 />
-                        <span> Game nhận biết màu sắc</span>
-                    </Link>
-                </li> */}
-
-                <hr className="sidebar-divider" />
-
-                <div className="sidebar-heading">
-                    Yêu cầu người dùng
-                </div>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to={`/request-student-off`}>
-                        <span>Nghỉ học</span>
+                        <span> Thời khóa biểu</span>
                     </Link>
                 </li>
 
                 <li className="nav-item">
-                    <Link className="nav-link" to={`/request-teacher-off`}>
-                        <span>Nghỉ dạy</span>
+                    <Link className="nav-link" to={`/turnovers`}>
+                        <BsCashCoin />
+                        <span> Trò chơi</span>
                     </Link>
                 </li>
 

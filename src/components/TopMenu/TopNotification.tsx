a2c71e22752364d83ce617eccb76d0ca35780e62
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-09-06 07:30:18 +0700

    add notification popup

--- src/components/TopMenu/TopNotification.tsx
+++ src/components/TopMenu/TopNotification.tsx
@@ -1,7 +1,6 @@
 import React, { useState, Dispatch } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/actions/account.actions";
-import { IoNotificationsSharp } from "react-icons/io5"
 
 function TopMenuNotification(): JSX.Element {
   const dispatch: Dispatch<any> = useDispatch();
@@ -11,35 +10,31 @@ function TopMenuNotification(): JSX.Element {
   return (
 
     <li className="nav-item dropdown no-arrow">
-      <a className="nav-link dropdown-toggle"
-        onClick={() => {
-          setShow(!isShow);
-        }}
-        href="# "
-        id="userDropdown"
-        role="button"
-        style={{color: "white"}}
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false">
-        <IoNotificationsSharp color="#1E90FF"/>
+
+
+      <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" onClick={() => {
+        setShow(!isShow);
+      }} href="# ">
+        <span className="badge badge-danger ml-2">4</span>
+        <i className="fas fa-bell"></i>
       </a>
 
-      <div className={`dropdown-menu dropdown-menu-right shadow animated--grow-in ${(isShow) ? "show" : ""}`}
+      <div className={`dropdown-menu dropdown-menu-right shadow dropdown-secondary animated--grow-in ${(isShow) ? "show" : ""}`}
         aria-labelledby="userDropdown">
-        <a className="dropdown-item"
-        onClick={() =>{}}
-        href="# " 
-        data-toggle="modal"
-        data-target="#logoutModal">
+        <a className="dropdown-item waves-light"
+          onClick={() => { }}
+          href="# "
+          data-toggle="modal"
+          data-target="#logoutModal">
           <i className="fas fa-bell fa-sm fa-fw mr-2 text-gray-400"></i>
           Yêu cầu nghỉ học
+          <span className="badge badge-danger ml-2">*</span>
         </a>
-        <a className="dropdown-item"
-        onClick={() => dispatch(logout())}
-        href="# " 
-        data-toggle="modal"
-        data-target="#logoutModal">
+        <a className="dropdown-item waves-light"
+          onClick={() => dispatch(logout())}
+          href="# "
+          data-toggle="modal"
+          data-target="#logoutModal">
           <i className="fas fa-bell fa-sm fa-fw mr-2 text-gray-400"></i>
           Logout
         </a>

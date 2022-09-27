d8b9884087d0b4fbdb9071d9b95bd38d8d1bb079
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-09-28 03:05:17 +0700

    fix detail course for teacher

--- src/components/Course/CourseTeacherDetail.tsx
+++ src/components/Course/CourseTeacherDetail.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, Dispatch, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IStateType, IRootPageStateType } from "../../store/models/root.interface";
 import { updateCurrentPath } from "../../store/actions/root.actions";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./CourseTeacherDetail.css"
 import { postRegisterTeachSemester } from "../../common/service/UserTeachSemester/PostRegisterTeachSemester";
 import { toast, ToastContainer } from "react-toastify";
@@ -11,27 +11,32 @@ const CourseTeacherDetail: React.FC = () => {
     const dispatch: Dispatch<any> = useDispatch();
     const path: IRootPageStateType = useSelector((state: IStateType) => state.root.page);
 
-    const { state } = useLocation<any>();
-    console.log(state)
-
     useEffect(() => {
         dispatch(updateCurrentPath("Khóa học", ""));
     }, [path.area, dispatch]);
 
     const history = useHistory();
 
+    var id_x = localStorage.getItem("description_course");
+    let description_course = "";
+    if (id_x !== null) {
+        description_course = id_x
+    }
+
+    var id_y = localStorage.getItem("semester_class_id");
+    let semester_class_id = 0;
+    if (id_y !== null) {
+        semester_class_id = parseInt(id_y)
+    }
+
     function handleRegister() {
-        if (state === undefined || state === null) {
-            return ""
-        }
-        else {
             dispatch(postRegisterTeachSemester({
                 teacher_id: localStorage.getItem('id'),
-                semester_class_id: state.semester_class_id
+                semester_classes_id: semester_class_id
             }))
             console.log({
                 teacher_id: localStorage.getItem('id'),
-                semester_class_id: state.semester_class_id
+                semester_classes_id: semester_class_id
             })
             toast.success("Đăng kí lớp thành công!", {
                 position: toast.POSITION.TOP_CENTER,
@@ -43,7 +48,6 @@ const CourseTeacherDetail: React.FC = () => {
                     pathname: path
                 });
             }, 2000); 
-        }
     }
 
     return (
@@ -51,18 +55,8 @@ const CourseTeacherDetail: React.FC = () => {
             <ToastContainer />
             <div className="col-xl-12 col-lg-12">
                 <div className="card shadow mb-4">
-                    {
-                        function () {
-                            if (state === undefined || state === null) {
-                                return ""
-                            }
-                            else {
-                                console.log(state)
-                                return <div className="card-body" dangerouslySetInnerHTML={{ __html: state.description }}>
-                                </div>
-                            }
-                        }()
-                    }
+                    <div className="card-body" dangerouslySetInnerHTML={{ __html: description_course }}>
+                    </div>
                     <div className="row text-center justify-content-center" id="btn-register-course">
                     <button className="btn btn-success btn-green" id="btn-create-register-course" onClick={() => handleRegister()}>
                         <i className="fas fa fa-plus"></i>

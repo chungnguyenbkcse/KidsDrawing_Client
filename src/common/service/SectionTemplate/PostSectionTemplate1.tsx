7811614bda8043f9ee61dcc5054b135936afdd19
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-10-31 19:28:45 +0700

    fix create course

---
+++ src/common/service/SectionTemplate/PostSectionTemplate1.tsx
@@ -0,0 +1,61 @@
+import { fetchDataRequest, fetchDataError } from "../../../store/actions/section_template.action";
+import { postRefreshToken } from "../Aut/RefreshToken";
+import { postTutorialTemplate } from "../TutorialTemplate/PostTutorialTemplate";
+import { postTutorialTemplate1 } from "../TutorialTemplate/PostTutorialTemplate1";
+import { getSectionTemplate } from "./GetSectionTemplate";
+
+export function postSectionTemplate1(data: any, num_of_section: number) {
+    var bearer = 'Bearer ' + localStorage.getItem("access_token");
+    
+    return (dispatch: any) => {
+        dispatch(fetchDataRequest());
+        fetch(
+                `${process.env.REACT_APP_API_URL}/section-template`, {
+                    method: "POST",
+                    headers: {
+                        'Authorization': bearer,
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': `${process.env.REACT_APP_API_LOCAL}`,
+                        'Access-Control-Allow-Credentials': 'true'
+                    },
+                    body: JSON.stringify(data)
+                }
+            )
+            .then( response => {
+                if (!response.ok) {
+                    if (response.status === 403) {
+                        dispatch(postRefreshToken())
+                        dispatch(postSectionTemplate1(data, num_of_section))
+                    }
+                    else {
+                        throw Error(response.statusText);
+                    }
+                }
+                else {
+                    return response.json()
+                }
+            })
+            .then (xx => {
+                console.log(xx)
+                getSectionTemplate(dispatch)
+                dispatch(postTutorialTemplate1({
+                        section_template_id: xx.id,
+                        name: data.name,
+                        creator_id: localStorage.getItem('id')
+                }))
+                if (data.number+1 <= num_of_section) {
+                    dispatch(postSectionTemplate1({
+                        creator_id: localStorage.getItem('id'),
+                        course_id: data.course_id,
+                        name: "",
+                        number: data.number + 1,
+                        teaching_form: ""
+                    }, num_of_section))
+                }
+            })
+            .catch(error => {
+                dispatch(fetchDataError(error));
+                console.log("error")
+            });
+    };
+}
--- src/common/service/TutorialTemplate/PostTutorialTemplate1.tsx
+++ src/common/service/TutorialTemplate/PostTutorialTemplate1.tsx
@@ -1,16 +1,15 @@
 import { toast } from "react-toastify";
-import { fetchDataRequest } from "../../../store/actions/course.action";
+import { fetchDataRequest } from "../../../store/actions/tutorial_template.action";
 import { postRefreshToken } from "../Aut/RefreshToken";
-import { postSectionTemplate } from "../SectionTemplate/PostSectionTemplate";
-import { postSectionTemplate1 } from "../SectionTemplate/PostSectionTemplate1";
-import { getCourse } from "./GetCourse";
+import { postTutorialTemplatePage } from "../TutorialTemplatePage/PostTutorialTemplatePage";
+import { getTutorialTemplate } from "./GetTutorialTemplate";
 
-export function postCourse(course: any, idx: any) {
+export function postTutorialTemplate1(data: any) {
     var bearer = 'Bearer ' + localStorage.getItem("access_token");
     return (dispatch: any) => {
         dispatch(fetchDataRequest());
         fetch(
-                `${process.env.REACT_APP_API_URL}/course`, {
+                `${process.env.REACT_APP_API_URL}/tutorial-template`, {
                     method: "POST",
                     headers: {
                         'Authorization': bearer,
@@ -18,14 +17,14 @@ export function postCourse(course: any, idx: any) {
                         'Access-Control-Allow-Origin': `${process.env.REACT_APP_API_LOCAL}`,
                         'Access-Control-Allow-Credentials': 'true'
                     },
-                    body: JSON.stringify(course)
+                    body: JSON.stringify(data)
                 }
             )
             .then( response => {
                 if (!response.ok) {
                     if (response.status === 403) {
                         dispatch(postRefreshToken())
-                        dispatch(postCourse(course, idx))
+                        dispatch(postTutorialTemplate1(data))
                     }
                     else {
                         throw Error(response.statusText);
@@ -35,20 +34,16 @@ export function postCourse(course: any, idx: any) {
                     return response.json()
                 }
             })
-            .then (data => {
-                console.log(data)
-                toast.update(idx, { render: "Thêm khóa học thành công", type: "success", isLoading: false, position: toast.POSITION.TOP_CENTER, autoClose: 2000 });
-                getCourse(dispatch)
-                dispatch(postSectionTemplate1({
-                    creator_id: localStorage.getItem('id'),
-                    course_id: data.id,
+            .then (xx => {
+                console.log(xx)
+                dispatch(postTutorialTemplatePage({
+                    tutorial_template_id: xx.id,
                     name: "",
-                    number: 1,
-                    teaching_form: ""
-                }, data.num_of_section))
+                    description: "",
+                    number: 1
+                }))
             })
             .catch(error => {
-                toast.update(idx, { render: "Thêm khóa học không thành công", type: "error", isLoading: false, position: toast.POSITION.TOP_CENTER, autoClose: 2000 });
                 console.log("error")
             });
     };

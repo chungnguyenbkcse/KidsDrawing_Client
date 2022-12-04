64063c8c9435b6b4cdaca9cc277aa6eb2362d795
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-12-04 11:34:36 +0700

    fix create feedback teacher

--- src/common/service/ClassHasRegisterJoinSemester/PutClassHasRegisterJoinSemesterTeacher.tsx
+++ src/common/service/ClassHasRegisterJoinSemester/PutClassHasRegisterJoinSemesterTeacher.tsx
@@ -1,10 +1,23 @@
 import { toast } from "react-toastify";
 import { fetchDataRequest } from "../../../store/actions/art_type.action";
 import { postRefreshToken } from "../Aut/RefreshToken";
+import { getInforClassHasRegisterJoinSemester } from "./GetInfoClassHasRegisterJoinSemester";
 
 export function putClassHasRegisterJoinSemesterTeacher(data: any, idx: any) {
     var bearer = 'Bearer ' + localStorage.getItem("access_token");
 
+    var id_x = localStorage.getItem('class_id');
+    let class_id: number = 0;
+    if (id_x !== null) {
+        class_id = parseInt(id_x)
+    }
+
+    var id_y = localStorage.getItem('student_id');
+    let student_id: number = 0;
+    if (id_y !== null) {
+        student_id = parseInt(id_y)
+    }
+    
     return (dispatch: any) => {
         dispatch(fetchDataRequest());
         fetch(
@@ -34,6 +47,7 @@ export function putClassHasRegisterJoinSemesterTeacher(data: any, idx: any) {
                 }
             })
             .then (res => {
+                getInforClassHasRegisterJoinSemester(dispatch, class_id, student_id)
                 toast.update(idx, { render: "Gửi nhận xét thành công", type: "success", isLoading: false, position: toast.POSITION.TOP_CENTER, autoClose: 2000 });
             })
             .catch(error => {

082e76091ae365c8bc501a6e9bd8c65f6391d7c3
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-12-20 09:59:55 +0700

    fix teacher list

--- src/components/Teachers/TeacherList.js
+++ src/components/Teachers/TeacherList.js
@@ -102,7 +102,7 @@ function TeacherList(props) {
       <span onClick={() => {
         onChangeRequest(row.id)
       }}>
-        <strong style={ { color: 'red', cursor: "pointer" } }>{teacher_level}/{total}</strong>
+        <strong style={ { color: 'red', cursor: "pointer" } }>{total}/{teacher_level}</strong>
       </span>
     );
   }

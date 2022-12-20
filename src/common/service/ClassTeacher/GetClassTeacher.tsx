85ba1d249cc4c4b9bd4dfed8a9facf4ca42d8233
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-12-20 11:59:30 +0700

    fix get class for teacher

--- src/common/service/ClassTeacher/GetClassTeacher.tsx
+++ src/common/service/ClassTeacher/GetClassTeacher.tsx
@@ -22,7 +22,7 @@ export function getClassTeacher(dispatch: any, id: any) {
     var bearer = 'Bearer ' + localStorage.getItem("access_token");
 
     return  fetch(
-                `${process.env.REACT_APP_API_URL}/classes/teacher/${id}`, {
+                `${process.env.REACT_APP_API_URL}/classes/teacher-list/${id}`, {
                     method: "GET",
                     headers: {
                         'Authorization': bearer,
@@ -50,9 +50,7 @@ export function getClassTeacher(dispatch: any, id: any) {
                 dispatch(fetchDataSuccess(data))
                 dispatch(removeDoinglClassAll())
                 dispatch(removeDoneClassAll())
-                console.log(data.body.classes_done)
-                console.log(data.body.classes_doing)
-                data.body.classes_done.map((ele: any, index: any) => {
+                data.body.classes.map((ele: any, index: any) => {
                     var class_teacher: ClassTeacher = {
                         id: ele.id,
                         name: ele.name,
@@ -72,41 +70,17 @@ export function getClassTeacher(dispatch: any, id: any) {
                         art_type_name: ele.art_type_name
                     }
                     console.log(class_teacher)
-                    if (index === 0){
-                        return dispatch(initialDoneClass(class_teacher));
-                    }
-                    else{
-                        return dispatch(addDoneClass(class_teacher))
-                    }
-                })
 
-                data.body.classes_doing.map((ele: any, index: any) => {
-                    var class_teacher: ClassTeacher = {
-                        id: ele.id,
-                        name: ele.name,
-                        link_url: ele.link_url,
-                        review_star: ele.review_star,
-                        course_id: ele.course_id,
-                        semester_name: ele.semester_name,
-                        semester_class_id: ele.semester_class_id,
-                        user_register_teach_semester: ele.user_register_teach_semester,
-                        security_code: ele.security_code,
-                        total_student: ele.total_student,
-                        num_of_section: ele.num_of_section,
-                        schedule: ele.schedule,
-                        course_name: ele.course_name,
-                        art_age_name: ele.art_age_name,
-                        art_level_name: ele.art_level_name,
-                        art_type_name: ele.art_type_name
-                    }
-                    console.log(class_teacher)
-                    if (index === 0){
-                        return dispatch(initialDoinglClass(class_teacher));
-                    }
-                    else{
+                    if (ele.status == 'DOING') {
                         return dispatch(addDoinglClass(class_teacher))
                     }
+                    else {
+                        return dispatch(addDoneClass(class_teacher))
+                    }
+                    
                 })
+
+                
             })
             .catch(error => {
                 dispatch(fetchDataError(error));

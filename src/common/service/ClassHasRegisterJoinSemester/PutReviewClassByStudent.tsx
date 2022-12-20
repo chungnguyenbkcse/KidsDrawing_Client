af85a56073fecbccee3008ce671168dc6c61c0b5
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-12-20 11:38:51 +0700

    fix review class for parent

--- src/common/service/ClassHasRegisterJoinSemester/PutReviewClassByStudent.tsx
+++ src/common/service/ClassHasRegisterJoinSemester/PutReviewClassByStudent.tsx
@@ -1,9 +1,16 @@
 import { toast } from "react-toastify";
 import { fetchDataRequest } from "../../../store/actions/art_type.action";
 import { postRefreshToken } from "../Aut/RefreshToken";
+import { getInforClassHasRegisterJoinSemester } from "./GetInfoClassHasRegisterJoinSemester";
 
 export function putReviewClassByStudent(data: any, idx: any) {
     var bearer = 'Bearer ' + localStorage.getItem("access_token");
+
+    var id_ix = localStorage.getItem('student_ids');
+    let student_ids: number[] = [];
+    if (id_ix !== null) {
+        student_ids = JSON.parse(id_ix);
+    }
 
     return (dispatch: any) => {
         dispatch(fetchDataRequest());
@@ -34,6 +41,10 @@ export function putReviewClassByStudent(data: any, idx: any) {
                 }
             })
             .then (res => {
+                if (student_ids.length > 0) {
+                    getInforClassHasRegisterJoinSemester(dispatch, data.classes_id, student_ids[0])
+                }
+                
                 toast.update(idx, { render: "Gửi nhận xét thành công", type: "success", isLoading: false, position: toast.POSITION.TOP_CENTER, autoClose: 1000 });
             })
             .catch(error => {
